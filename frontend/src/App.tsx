a66a2583ee7a3db63d49ccd05fc48dcf2360de8c
goyalpramod <goyalpramod1729@gmail.com>
2025-11-14 21:04:01 -0500

    experimenting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,57 +1,71 @@
 import { useState, useCallback } from 'react';
-import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge } from '@xyflow/react';
+import {
+  ReactFlow,
+  Controls,
+  Background,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+} from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
- 
+import { TextUpdaterNode } from './components/Nodes';
+
+const nodeTypes = {
+  textUpdater: TextUpdaterNode,
+};
+
 const initialNodes = [
   {
     id: 'n1',
-    position: { x: 0, y: 0 },
     data: { label: 'Node 1' },
-    type: 'input',
+    position: { x: 0, y: 0 },
+    type: 'textUpdater',
   },
   {
     id: 'n2',
-    position: { x: 100, y: 100 },
     data: { label: 'Node 2' },
+    position: { x: 100, y: 100 },
   },
 ];
- 
+
 const initialEdges = [
-  {
-    id: 'n1-n2',
-    source: 'n1',
-    target: 'n2',
-  },
+  { id: 'n1-n2', source: 'n1', target: 'n2', label: 'connects with' },
 ];
 
-
-export default function App() {
+function Flow() {
   const [nodes, setNodes] = useState(initialNodes);
   const [edges, setEdges] = useState(initialEdges);
- 
+
   const onNodesChange = useCallback(
-    (changes) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
     [],
   );
   const onEdgesChange = useCallback(
-    (changes) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
     [],
   );
+
   const onConnect = useCallback(
     (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
     [],
   );
- 
+
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div style={{ height: '100%' }}>
       <ReactFlow
         nodes={nodes}
         edges={edges}
+        nodeTypes={nodeTypes}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         fitView
-      />
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
     </div>
   );
 }
+
+export default Flow;
---
+++ frontend/src/components/Nodes.tsx
@@ -0,0 +1,19 @@
+import { useCallback } from 'react';
+import { Handle, Position } from '@xyflow/react';
+
+export function TextUpdaterNode(props) {
+  const onChange = useCallback((evt) => {
+    console.log(evt.target.value);
+  }, []);
+
+  return (
+    <div className="text-updater-node">
+      <Handle type="target" position={Position.Top} />
+      <div>
+        <label htmlFor="text">Text:</label>
+        <input id="text" name="text" onChange={onChange} className="nodrag" />
+      </div>
+      <Handle type="source" position={Position.Bottom} />
+    </div>
+  );
+}
